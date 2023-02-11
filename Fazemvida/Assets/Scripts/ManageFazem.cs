b084a16cfde8f2409735e3b2af9dccbded273148
ChloeLidec <chloe.lidec@etu.univ-orleans.fr>
2023-02-10 19:00:59 -0500

    #1 - Correction, la scene unload seule

--- Fazemvida/Assets/Scripts/ManageFazem.cs
+++ Fazemvida/Assets/Scripts/ManageFazem.cs
@@ -8,7 +8,6 @@ public class ManageFazem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.UnloadSceneAsync("ConnexionScene");
         
     }
 
